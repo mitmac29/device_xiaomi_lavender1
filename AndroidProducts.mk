b4e100a699ed270b9586ab6ed8236992146719e8
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2023-05-04 10:54:11 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/carbon_lavender.mk
+    $(LOCAL_DIR)/lineage_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    carbon_lavender-user \
-    carbon_lavender-userdebug \
-    carbon_lavender-eng
+    lineage_lavender-user \
+    lineage_lavender-userdebug \
+    lineage_lavender-eng
