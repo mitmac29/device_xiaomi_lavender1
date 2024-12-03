f0182b4afe997335110052dd8901ad24668d4859
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2024-12-04 02:35:45 +0530

    Update lineage_lavender.mk

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common LineageOS stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 720
 TARGET_DISABLE_BLUR := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
