1792947578692fa70aba28d710239c3640805007
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2024-12-22 23:37:51 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,6 +4,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Audio (Dolby Atmos)
+$(call inherit-product, vendor/motorola-dolby/setup.mk)
+
+# V4A
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/lavender/lavender-vendor.mk)
 
